40500b8a3c9e7fbc24fd98f1074ea9ba67f0c294
Muhammad Abdullah Khan <abdullahkhan.smiu@gmail.com>
2021-11-05 18:10:59 +0000

    Changes Made

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -6,6 +6,7 @@ import 'package:budget_app/models/expense_model.dart';
 import 'package:budget_app/widgets/bar_chart.dart';
 import 'package:flutter/material.dart';
 
+import 'category_screen.dart';
 import 'colors/colorhelper.dart';
 
 class HomePage extends StatefulWidget {
@@ -17,64 +18,68 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   _buildCategory(Category category, double totalAmount) {
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-      height: 100,
-      width: double.infinity,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: [
-          BoxShadow(
-              color: Colors.black12, offset: Offset(0, 2), blurRadius: 6.0)
-        ],
-        color: Colors.white,
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-            Text(
-              category.name,
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+    return GestureDetector(
+      onTap: () => Navigator.push(context,
+          MaterialPageRoute(builder: (_) => CategoryScreen(cat: category))),
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        height: 100,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black12, offset: Offset(0, 2), blurRadius: 6.0)
+          ],
+          color: Colors.white,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              Text(
+                category.name,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+              ),
+              Text(
+                "\$${(category.maxAmount - totalAmount).toStringAsFixed(2)}/\$${category.maxAmount}",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+              )
+            ]),
+            SizedBox(
+              height: 10,
             ),
-            Text(
-              "\$${(category.maxAmount - totalAmount).toStringAsFixed(2)}/\$${category.maxAmount}",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-            )
-          ]),
-          SizedBox(
-            height: 10,
-          ),
-          LayoutBuilder(
-            builder: (BuildContext context, BoxConstraints constraints) {
-              final double maxBarW = constraints.maxWidth;
-              final double percent =
-                  (category.maxAmount - totalAmount) / category.maxAmount;
-              double barWidth = percent * maxBarW;
-              if (barWidth < 0) {
-                barWidth = 0;
-              }
-              return Stack(
-                children: [
-                  Container(
-                    height: 20,
-                    decoration: BoxDecoration(
-                      color: Colors.grey[200],
-                      borderRadius: BorderRadius.circular(15),
+            LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraints) {
+                final double maxBarW = constraints.maxWidth;
+                final double percent =
+                    (category.maxAmount - totalAmount) / category.maxAmount;
+                double barWidth = percent * maxBarW;
+                if (barWidth < 0) {
+                  barWidth = 0;
+                }
+                return Stack(
+                  children: [
+                    Container(
+                      height: 20,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(15),
+                      ),
                     ),
-                  ),
-                  Container(
-                    height: 20,
-                    width: barWidth,
-                    decoration: BoxDecoration(
-                        color: getColor(context, percent),
-                        borderRadius: BorderRadius.circular(15)),
-                  )
-                ],
-              );
-            },
-          )
-        ],
+                    Container(
+                      height: 20,
+                      width: barWidth,
+                      decoration: BoxDecoration(
+                          color: getColor(context, percent),
+                          borderRadius: BorderRadius.circular(15)),
+                    )
+                  ],
+                );
+              },
+            )
+          ],
+        ),
       ),
     );
   }
